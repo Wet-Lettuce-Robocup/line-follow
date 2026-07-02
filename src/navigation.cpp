074e69d952e82fbb2da32513a36b7746dd85fabc
William D'Olier <william@dolier.net>
2026-07-02 15:56:55 +1000

    removed double green logic

--- src/navigation.cpp
+++ src/navigation.cpp
@@ -424,19 +424,19 @@ double NavigationNode::simpleError(const cv::Mat & frame)
     // --- Draw green contours in green ---
   cv::drawContours(processed, greenContours, -1, cv::Scalar(0, 255, 0), 2);
 
-  if (greenCenters.size() >= 2) {
-    cv::Point p1 = greenCenters[0];
-    cv::Point p2 = greenCenters[1];
-
-    double angle = this->calculateAngle(p1, p2);
-    if (std::abs(angle) < 0.5 || std::abs(angle) > std::numbers::pi - 0.5) {
-      RCLCPP_INFO(this->get_logger(), "Starting double green");
-      this->sendMovementGoal(0, 100, 5);
-      this->state = GREEN_ROTATE;
-      RCLCPP_INFO(this->get_logger(), "Sent double green start message");
-      return 0;
-    }
-  }
+  // if (greenCenters.size() >= 2) {
+  //   cv::Point p1 = greenCenters[0];
+  //   cv::Point p2 = greenCenters[1];
+  //
+  //   double angle = this->calculateAngle(p1, p2);
+  //   if (std::abs(angle) < 0.5 || std::abs(angle) > std::numbers::pi - 0.5) {
+  //     RCLCPP_INFO(this->get_logger(), "Starting double green");
+  //     this->sendMovementGoal(0, 100, 5);
+  //     this->state = GREEN_ROTATE;
+  //     RCLCPP_INFO(this->get_logger(), "Sent double green start message");
+  //     return 0;
+  //   }
+  // }
 
   cv::Point2d weightedSum = lineCentroid;
   double totalWeight = 1.0;
